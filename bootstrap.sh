5da433da90deec20265a89e597f3d4e4ded213bc
jekkos-ds213 <jeroen.peelaerts@gmail.com>
2013-10-16 22:59:02 +0200

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/opt/bin/bash
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin syno 
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+	[ -f ~/.bash_profile ] && source ~/.bash_profile
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
