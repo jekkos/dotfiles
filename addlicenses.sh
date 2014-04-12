c83bf8b979c0a9319942b4727316f1913145530a
jekkos-ds213 <jeroen.peelaerts@gmail.com>
2014-04-12 16:35:41 +0200

    Update license script

--- addlicenses.sh
+++ addlicenses.sh
@@ -1,2 +1,4 @@
-#!bin/sh
-/usr/syno/pgsql/bin/psql -d surveillance -U admin -c "INSERT INTO licensekey (camera_count, key, serial) VALUES (10, 2, '');"
+#!/bin/sh
+#/usr/syno/pgsql/bin/psql -d surveillance -U admin -c "INSERT INTO licensekey (camera_count, key, serial) VALUES (10, 2, 'RQQM8-4XMKD-7VB23-FMQYB');"
+#/usr/syno/pgsql/bin/psql -d surveillance -U admin -c "INSERT INTO licensekey (camera_count, key, serial) VALUES (10, 2, 'RW7MH-MTCDR-CFJ62-T8M7M');"
+/usr/syno/pgsql/bin/psql -d surveillance -U admin -c "INSERT INTO licensekey (camera_count, key, serial) VALUES (10, 2, 'FX4JR-34F49-VXHH4-MQC3Y');"
